edafc47b26f7993cc81adcc1e5f2fceb774ac534
Ulises Lopez Pacholczak <Ulises@Air-de-facundo.fibertel.com.ar>
2020-12-30 10:36:09 -0300

    Ejemplo de cargar preguntas logrado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import RouterComponent from './components/Router';
 
+import {loadQuestions} from './redux/slices/questions'
+import {useSelector, useDispatch} from 'react-redux';
+
 function App() {
+	const dispatch = useDispatch();
+	
+	React.useEffect (() => {
+		dispatch(loadQuestions());
+	}, []);
+
 	return (
 		<div className='App'>
 			<RouterComponent />
